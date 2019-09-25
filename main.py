a25420ae23e4a38d75000e2abed643af26d9557f
mrudu <lou.jarak@gmail.com>
2019-09-25 10:29:13 +0530

    removed unnecessary print statements

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 import keras
+from keras import backend as K
 import numpy as np
 from keras.models import model_from_json
 from PIL import Image
@@ -19,6 +20,14 @@ CORS(app)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+file = request.files['mango_pic']
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+mango_trainer = model_from_json(loaded_model_json)
+# load weights into new model
+mango_trainer.load_weights("model.h5")
 
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -51,7 +60,21 @@ def get_mango_data(index):
 		json_data["texture_mango"] = "shrivelled and smelly"
 		json_data["life_left"] = "0 days"
 	return json_data
+
+def get_prediction(filename):
+	img = Image.open('uploads/'+filename).resize((256, 256))
+	img =  np.array(img)/255.0
+	test_images = []
+	test_images.append(img)
+	test_images = np.array(test_images)
+
+	prediction = list(list(mango_trainer.predict(test_images))[0])
+	mango_data = get_mango_data(prediction.index(max(prediction)))
+	return mango_data
 	
+
+# Route Handlers
+
 @app.route('/', methods=['GET'])
 def upload_form():
 	return render_template('upload.html')
@@ -59,42 +82,19 @@ def upload_form():
 @app.route('/', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
-		print(request.files.to_dict(flat=False))
-		print(request.args)
-		print(request.data)
-		print(request.get_json())
-		print(request.form)
-		print(request.get_data())
+		# get form data parameter
+		form_data = request.form.to_dict(flat=False)
         # check if the post request has the file part
 		if 'mango_pic' not in request.files:
 			flash('No file part')
 			return jsonify(data="")
-		file = request.files['mango_pic']
-		json_file = open('model.json', 'r')
-		loaded_model_json = json_file.read()
-		json_file.close()
-		mango_trainer = model_from_json(loaded_model_json)
-		# load weights into new model
-		mango_trainer.load_weights("model.h5")
 		if file.filename == '':
 			flash('No file selected for uploading')
 			return jsonify(data="")
 		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
+			filename = secure_filename(form_data["fileName"])
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-			img = Image.open('uploads/'+filename).resize((256, 256))
-			img =  np.array(img)/255.0
-			test_images = []
-			test_images.append(img)
-			test_images = np.array(test_images)
-
-			prediction = list(list(mango_trainer.predict(test_images))[0])
-			print(prediction)
-			print(max(prediction))
-			mango_data = get_mango_data(prediction.index(max(prediction)))
-			print(prediction.index(max(prediction)))
-			print(mango_data)
+			mango_data = get_prediction(filename)
 			flash('File successfully uploaded -  It is a ' + mango_data["type_mango"] + " mango")
 			return jsonify(data=mango_data)
 		else:
